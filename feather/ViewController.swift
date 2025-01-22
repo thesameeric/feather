6bd9ee8919e4e800b53d30019693c323a74594b4
Eric O Okiti <osivwiokiti@gmail.com>
2025-01-22 05:14:46 +0100

    Initial Commit

---
+++ feather/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  feather
+//
+//  Created by Osivwi Eric Okiti on 22/01/2025.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ featherTests/featherTests.swift
@@ -0,0 +1,17 @@
+//
+//  featherTests.swift
+//  featherTests
+//
+//  Created by Osivwi Eric Okiti on 22/01/2025.
+//
+
+import Testing
+@testable import feather
+
+struct featherTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
